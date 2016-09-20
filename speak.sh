26ded3f7f92d819de66e92dce8aa0981be86bb88
hopesandshadows <r.etteridge+github@gmail.com>
2016-09-20 11:25:44 +0100

    Update speak.sh

--- speak.sh
+++ speak.sh
@@ -1,5 +1,5 @@
 #install
 sudo apt-get install espeak rsstail rsstail -u http://www.reddit.com/r/showerthoughts/.rss | festival --tts
-
-begin
+wait 30
+#begin
 rsstail -u http://www.reddit.com/r/showerthoughts/.rss | espeak -ven+whisper -k5 -s150
